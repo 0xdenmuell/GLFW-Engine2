05954235081c86e76e7c400517a844f4e6a3f239
Dennis Müller - Surface <dennis25mueller@yahoo.de>
2020-04-17 05:28:55 +0200

    - added glad - added first render feature - deleted old files - renamed files

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,6 +1,9 @@
-#include "include/GLFW/glfw3.h"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 #include <iostream>
 #include <stdlib.h>
+
+#define LOG(msg) std::cout << msg << std::endl;
 
 void error_callback(int error, const char* description)
 {
@@ -16,38 +19,63 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+
+	if (key != 0 && action == GLFW_PRESS)
+	{
+		std::cout << glfwGetKeyName(key, scancode);
+	}
 }
+
+void window_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
 
 int main(void)
 {
 	glfwSetErrorCallback(error_callback);
 
-	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
 	GLFWwindow* window = glfwCreateWindow(800, 800, "GLFW Window", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
 	glfwSetKeyCallback(window, key_callback);
 
-	int counter = 0;
+	glViewport(0, 0, 800, 800);
 
+	glfwSetFramebufferSizeCallback(window, window_size_callback);
+
+
+	
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwPollEvents();
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();		
 		// Keep running
 	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
-
 
 	return 0;
 }
--- Engine/test.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <iostream>
-
-#include "GLFW/glfw-3.3.2/include/GLFW/glfw3.h"
