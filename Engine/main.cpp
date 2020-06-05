60799b55bca4b862f971ad505a7b99e8e4f7d4a7
Dennis Müller - Surface <dennis25mueller@yahoo.de>
2020-06-05 02:38:43 +0200

    Introducing textures

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,9 +1,134 @@
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdlib.h>
+#include <shader/shader.h>
+#include <stb-master/stb_image.h>
 
 #define LOG(msg) std::cout << msg << std::endl;
+
+void error_callback(int error, const char* description);
+
+void timer(double duration);
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+void window_size_callback(GLFWwindow* window, int width, int height);
+
+unsigned int SCR_WIDTH = 800;
+unsigned int SCR_HEIGHT = 800;
+
+int main(void)
+{
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit())
+		return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "GLFW Window", NULL, NULL);
+	if (!window)
+	{
+		LOG("Failed to initialize GLFW")
+			glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		LOG("Failed to initialize GLAD");
+		return -1;
+	}
+	glfwSetKeyCallback(window, key_callback);
+
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+	Shader ShaderProgram("C:\\dev\\GLFW-Engine\\GLFW-Engine2\\Engine\\vertices.glsl",
+						 "C:\\dev\\GLFW-Engine\\GLFW-Engine2\\Engine\\fragment.glsl");
+
+
+
+	float vertices[] = {
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,	0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,	1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,	0.5f, 1.0f   // top 
+	};
+	
+	//bind buffer
+	unsigned int VBO, VAO, texture;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenTextures(1, &texture);
+
+	glBindVertexArray(VAO);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	if (true)
+	{
+		glTexImage2D(GL_TEXTURE, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(2 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	
+
+
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//glBindVertexArray(0);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	glfwSetFramebufferSizeCallback(window, window_size_callback);
+
+	glfwSetWindowSizeCallback(window, window_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ShaderProgram.use();
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		// Keep running
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
 
 void error_callback(int error, const char* description)
 {
@@ -29,154 +154,8 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 void window_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
-
-
-int main(void)
-{
-	glfwSetErrorCallback(error_callback);
-
-	if (!glfwInit())
-		return -1;
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
-	GLFWwindow* window = glfwCreateWindow(800, 800, "GLFW Window", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-
-	glfwSetKeyCallback(window, key_callback);
-
-	glViewport(0, 0, 800, 800);
-
-	///compile errors
-	int successCompile;
-	char infoLogCompile[512];
-
-	///create shader
-	const char* vertexShaderSource = 
-		"#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\0";
-
-	unsigned int shaderID;
-	shaderID = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(shaderID, 1, &vertexShaderSource, NULL);
-	glCompileShader(shaderID);
-	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &successCompile);
-
-	if (!successCompile)
-	{
-		glGetShaderInfoLog(shaderID, 512, NULL, infoLogCompile);
-		std::cout	<< "compilation error for shaderID: " << shaderID << std::endl
-					<< "error log: " << infoLogCompile;
-	}
-
-	///create fragment
-	const char* fragmentShaderSource =
-		"#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0);\n"
-		"}\0";
-
-	unsigned int fragmentID;
-	fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentID, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentID);
-	glGetShaderiv(fragmentID, GL_COMPILE_STATUS, &successCompile);
-
-	if (!successCompile)
-	{
-		char infoLogCompile[512];
-		glGetShaderInfoLog(fragmentID, 512, NULL, infoLogCompile);
-		std::cout	<< "compilation error for fragmentID: " << fragmentID << std::endl
-					<< "error log: " << infoLogCompile;
-
-	}
-
-	///create linking program
-
-	unsigned int shaderProgramID;
-	shaderProgramID = glCreateProgram();
-
-	glAttachShader(shaderProgramID, shaderID);
-	glAttachShader(shaderProgramID, fragmentID);
-	glLinkProgram(shaderProgramID);
-
-	glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &successCompile);
-
-	if (!successCompile)
-	{
-		char infoLogCompile[512];
-		glGetShaderInfoLog(shaderProgramID, 512, NULL, infoLogCompile);
-		std::cout << "compilation error for shaderProgramID: " << shaderProgramID << std::endl
-			<< "error log: " << infoLogCompile;
-
-	}
-
-	glDeleteShader(shaderID);
-	glDeleteShader(fragmentID);
-
-	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
-	};
-
-
-	//bind buffer
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * (sizeof(float)), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glBindVertexArray(0);
-
-	glfwSetFramebufferSizeCallback(window, window_size_callback);
-
-	while (!glfwWindowShouldClose(window))
-	{
-
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glUseProgram(shaderProgramID);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-		// Keep running
-	}
-
-	glfwDestroyWindow(window);
-	glfwTerminate();
-
-	return 0;
+	SCR_HEIGHT = height;
+	SCR_WIDTH = width;
+	LOG(SCR_HEIGHT); LOG(SCR_WIDTH);
+	glViewport(0, 0, SCR_HEIGHT, SCR_WIDTH);
 }
